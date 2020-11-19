be41853d976ef08d84865a220e22ddf90a766b33
mrchef25 <74696309+mrchef25@users.noreply.github.com>
2020-11-19 04:08:39 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'Nzc4NzU5Mzk3MzI4ODE0MDgw.X7WqNA.HhUWUmq75kJ83jxUa6nz7rKHY-s';
+
+bot.on('ready', () => {
+    console.log('Botas veikia');
+})
+
+
+bot.login(token);
+
+bot.on('message', message => {
+
+    if(message.content.toLowerCase() === '!ausines')
+    message.reply('VIPER V380 RGB 7.1 SURROUND SOUND GAMING HEADSET');
+
+    else if(message.content.toLowerCase() === '!gpu')
+       message.reply('NVIDIA GeForce GTX 1650 4gb');
+
+    else if(message.content.toLowerCase() === '!cpu')
+       message.reply('Intel(R) Core(TM) i7-2600 @ 3.40GHz 4 Core(s) (8 logical CPUs)');
+
+    else if(message.content.toLowerCase() === '!mic')
+       message.reply('The HyperX QuadCast™');
+       
+    else if(message.content.toLowerCase() === '!mobo')
+       message.reply('Lenovo Mahobay');
+
+    else if(message.content.toLowerCase() === '!ram')
+       message.reply('XUM 16gb DDR3 1866MHz (PC3-14900)');
+
+    else if(message.content.toLowerCase() === '!cam')
+       message.reply('Canon EOS M50 Mirrorless Camera su EF-M 15-45 mm f/3.5-5.6 IS STM lens)');
+
+
+});
+
